aa80e7c45f303c425807278ecc50e32937f0f49a
Fernando Ollé <55040050+fernandOlle@users.noreply.github.com>
2022-03-10 23:57:30 -0300

    Update NavbarElements.jsx

--- src/components/Navbar/NavbarElements.jsx
+++ src/components/Navbar/NavbarElements.jsx
@@ -15,7 +15,7 @@ export const Nav = styled.nav`
   top: 0;
   z-index: 10;
 
-  @media screen and (max-width: 960px) {
+  @media screen and (max-width: 8000px) {
     transition: 0.8s all ease;
   }
 `;
